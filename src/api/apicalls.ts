ab56555043f7807ec9eea6bfd48636ce78ac353c
VDUNGKMA <dungnguyenkma050402@gmail.com>
2024-09-07 11:17:29 +0700

    first commit

---
+++ src/api/apicalls.ts
@@ -0,0 +1,17 @@
+//const apikey: string = '753049c9feaee5416b7f1b3fe918f733';
+const apikey: string = '5e917011565515815c286b6ef6e387ef'
+export const baseImagePath = (size: string, path: string) => {
+  return `https://image.tmdb.org/t/p/${size}${path}`;
+};
+export const nowPlayingMovies: string = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}`;
+export const upcomingMovies: string = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apikey}`;
+export const popularMovies: string = `https://api.themoviedb.org/3/movie/popular?api_key=${apikey}`;
+export const searchMovies = (keyword: string) => {
+  return `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${keyword}`;
+};
+export const movieDetails = (id: number) => {
+  return `https://api.themoviedb.org/3/movie/${id}?api_key=${apikey}`;
+};
+export const movieCastDetails = (id: number) => {
+  return `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apikey}`;
+};
