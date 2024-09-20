95724d1ce26c92a495253127af45569b4a8b95cf
thuphuong11 <ninhthuphuong9311@gmail.com>
2024-09-21 00:16:19 +0700

    09/21

--- src/screens/InfoScreen.tsx
+++ src/screens/InfoScreen.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput } from 'react-native';
-import { COLORS, FONTSIZE, SPACING, FONTFAMILY } from '../theme/theme';
-import Icon from 'react-native-vector-icons/Feather'; // Sử dụng Feather Icons
+import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput, Image } from 'react-native';
+import { COLORS, FONTSIZE, SPACING, FONTFAMILY } from '../theme/theme'; // Đường dẫn đến theme của bạn
 
 const InfoScreen = ({ route, navigation }: any) => {
     const { user } = route.params; // Lấy thông tin người dùng từ route.params
@@ -28,7 +27,10 @@ const InfoScreen = ({ route, navigation }: any) => {
             <View style={styles.textBox}>
                 <Text style={styles.infoLabel}>Username: {newUsername}</Text>
                 <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.editIcon}>
-                    <Icon name="edit" size={20} color={COLORS.Black} />
+                    <Image
+                        source={require('../assets/image/pencil-square.png')} // Đường dẫn đến ảnh icon
+                        style={styles.icon} // Định dạng style cho icon
+                    />
                 </TouchableOpacity>
             </View>
 
@@ -72,100 +74,104 @@ const InfoScreen = ({ route, navigation }: any) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: SPACING.space_20,
-    backgroundColor: COLORS.Black, // Nền màu đen
-  },
-  title: {
-    fontFamily: FONTFAMILY.poppins_medium,
-    fontSize: FONTSIZE.size_24,
-    marginBottom: SPACING.space_20,
-    color: COLORS.White, // Text màu trắng để dễ đọc trên nền đen
-  },
-  textBox: {
-    backgroundColor: COLORS.White, // Text box màu trắng
-    padding: SPACING.space_12,
-    borderRadius: 8,
-    marginBottom: SPACING.space_15,
-    shadowColor: 'rgba(0, 0, 0, 0.4)', // Đổ bóng với màu mờ hơn để tạo sự mềm mại
-    shadowOffset: { width: 0, height: 6 }, // Vị trí đổ bóng
-    shadowOpacity: 0.3, // Độ mờ của bóng để tạo hiệu ứng nhẹ hơn
-    shadowRadius: 10, // Bán kính đổ bóng lớn hơn
-    elevation: 12, // Đổ bóng cho Android
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  infoLabel: {
-    fontFamily: FONTFAMILY.poppins_regular,
-    fontSize: FONTSIZE.size_16,
-    color: COLORS.Black, // Text màu đen trên nền trắng của text box
-  },
-  editIcon: {
-    marginLeft: 10,
-  },
-  button: {
-    marginTop: SPACING.space_20,
-    backgroundColor: COLORS.Orange, // Nút màu cam
-    paddingVertical: SPACING.space_12,
-    paddingHorizontal: SPACING.space_24,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: COLORS.White, // Text màu trắng trên nút cam
-    fontFamily: FONTFAMILY.poppins_medium,
-    fontSize: FONTSIZE.size_16,
-    textAlign: 'center',
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.7)', // Nền trong suốt mờ
-  },
-  modalContent: {
-    backgroundColor: COLORS.White, // Nền trắng cho modal
-    padding: SPACING.space_20,
-    borderRadius: 8,
-    width: '80%',
-  },
-  modalTitle: {
-    fontFamily: FONTFAMILY.poppins_medium,
-    fontSize: FONTSIZE.size_20,
-    marginBottom: SPACING.space_12,
-    color: COLORS.Black,
-    textAlign: 'center',
-  },
-  input: {
-    borderColor: COLORS.Grey,
-    borderWidth: 1,
-    padding: SPACING.space_12,
-    borderRadius: 8,
-    marginBottom: SPACING.space_20,
-    color: COLORS.Black,
-  },
-  saveButton: {
-    backgroundColor: COLORS.Orange,
-    paddingVertical: SPACING.space_12,
-    borderRadius: 8,
-  },
-  saveButtonText: {
-    color: COLORS.White,
-    textAlign: 'center',
-    fontFamily: FONTFAMILY.poppins_medium,
-  },
-  cancelButton: {
-    marginTop: SPACING.space_12,
-    backgroundColor: COLORS.Grey,
-    paddingVertical: SPACING.space_12,
-    borderRadius: 8,
-  },
-  cancelButtonText: {
-    color: COLORS.White,
-    textAlign: 'center',
-    fontFamily: FONTFAMILY.poppins_medium,
-  },
+    container: {
+        flex: 1,
+        padding: SPACING.space_20,
+        backgroundColor: COLORS.Black, // Nền màu đen
+    },
+    title: {
+        fontFamily: FONTFAMILY.poppins_medium,
+        fontSize: FONTSIZE.size_24,
+        marginBottom: SPACING.space_20,
+        color: COLORS.White, // Text màu trắng để dễ đọc trên nền đen
+    },
+    textBox: {
+        backgroundColor: COLORS.White, // Text box màu trắng
+        padding: SPACING.space_12,
+        borderRadius: 8,
+        marginBottom: SPACING.space_15,
+        shadowColor: 'rgba(0, 0, 0, 0.4)', // Đổ bóng với màu mờ hơn để tạo sự mềm mại
+        shadowOffset: { width: 0, height: 6 }, // Vị trí đổ bóng
+        shadowOpacity: 0.3, // Độ mờ của bóng để tạo hiệu ứng nhẹ hơn
+        shadowRadius: 10, // Bán kính đổ bóng lớn hơn
+        elevation: 12, // Đổ bóng cho Android
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    infoLabel: {
+        fontFamily: FONTFAMILY.poppins_regular,
+        fontSize: FONTSIZE.size_16,
+        color: COLORS.Black, // Text màu đen trên nền trắng của text box
+    },
+    editIcon: {
+        marginLeft: 10,
+    },
+    icon: {
+        width: 24,  // Chiều rộng icon
+        height: 24, // Chiều cao icon
+    },
+    button: {
+        marginTop: SPACING.space_20,
+        backgroundColor: COLORS.Orange, // Nút màu cam
+        paddingVertical: SPACING.space_12,
+        paddingHorizontal: SPACING.space_24,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: COLORS.White, // Text màu trắng trên nút cam
+        fontFamily: FONTFAMILY.poppins_medium,
+        fontSize: FONTSIZE.size_16,
+        textAlign: 'center',
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.7)', // Nền trong suốt mờ
+    },
+    modalContent: {
+        backgroundColor: COLORS.White, // Nền trắng cho modal
+        padding: SPACING.space_20,
+        borderRadius: 8,
+        width: '80%',
+    },
+    modalTitle: {
+        fontFamily: FONTFAMILY.poppins_medium,
+        fontSize: FONTSIZE.size_20,
+        marginBottom: SPACING.space_12,
+        color: COLORS.Black,
+        textAlign: 'center',
+    },
+    input: {
+        borderColor: COLORS.Grey,
+        borderWidth: 1,
+        padding: SPACING.space_12,
+        borderRadius: 8,
+        marginBottom: SPACING.space_20,
+        color: COLORS.Black,
+    },
+    saveButton: {
+        backgroundColor: COLORS.Orange,
+        paddingVertical: SPACING.space_12,
+        borderRadius: 8,
+    },
+    saveButtonText: {
+        color: COLORS.White,
+        textAlign: 'center',
+        fontFamily: FONTFAMILY.poppins_medium,
+    },
+    cancelButton: {
+        marginTop: SPACING.space_12,
+        backgroundColor: COLORS.Grey,
+        paddingVertical: SPACING.space_12,
+        borderRadius: 8,
+    },
+    cancelButtonText: {
+        color: COLORS.White,
+        textAlign: 'center',
+        fontFamily: FONTFAMILY.poppins_medium,
+    },
 });
 
 export default InfoScreen;
